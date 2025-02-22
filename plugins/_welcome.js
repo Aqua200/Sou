d521a51c43395d0621032aa50c88559f8327381f
Aqua200 <chinquepapa@gmail.com>
2025-02-22 11:36:28 -0800

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,13 +1,21 @@
-import { WAMessageStubType } from '@whiskeysockets/baileys'
-import fetch from 'node-fetch'
+import { WAMessageStubType } from '@whiskeysockets/baileys';
+import fetch from 'node-fetch';
 
 export async function before(m, { conn, participants, groupMetadata }) {
-  if (!m.messageStubType || !m.isGroup) return true
+  if (!m.messageStubType || !m.isGroup) return true;
 
-  let who = m.messageStubParameters[0]
-  let taguser = `@${who.split('@')[0]}`
-  let chat = global.db.data.chats[m.chat]
-  let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg';
+  let who = m.messageStubParameters[0];
+  let taguser = `@${who.split('@')[0]}`;
+  let chat = global.db.data.chats[m.chat];
+
+  // Arreglo de 5 imágenes
+  const images = [
+    'https://qu.ax/uVvOx.jpeg',
+    'https://qu.ax/RHADB.jpeg',
+    'https://qu.ax/Rndis.jpeg',
+    'https://qu.ax/pYVdH.jpeg',
+    'https://qu.ax/doymM.jpeg',
+  ];
 
   if (chat.welcome) {
     let img;
@@ -15,17 +23,19 @@ export async function before(m, { conn, participants, groupMetadata }) {
       let pp = await conn.profilePictureUrl(who, 'image');
       img = await (await fetch(pp)).buffer();
     } catch {
-      img = await (await fetch(defaultImage)).buffer();
+      // Seleccionar una imagen aleatoria del arreglo
+      const randomImage = images[Math.floor(Math.random() * images.length)];
+      img = await (await fetch(randomImage)).buffer();
     }
 
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `❀ *Bienvenido* a ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom1}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
-      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
+      let bienvenida = `❀ *Bienvenido* a `${{groupMetadata.subject}\n ✰}$`{taguser}\n${global.welcom1}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`;
+      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] });
     } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
-      let bye = `❀ *Adiós* De ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
-      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
+      let bye = `❀ *Adiós* De `${{groupMetadata.subject}\n ✰}$`{taguser}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`;
+      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] });
     }
   }
 
-  return true
+  return true;
 }
