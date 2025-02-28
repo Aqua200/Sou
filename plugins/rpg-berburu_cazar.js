3ac7c85af21204ee01e164d4b9a55b784bc9001d
Aqua200 <chinquepapa@gmail.com>
2025-02-28 01:05:30 -0800

    Update rpg-berburu_cazar.js

--- plugins/rpg-berburu_cazar.js
+++ plugins/rpg-berburu_cazar.js
@@ -1,36 +1,40 @@
 let cooldowns = {}
 
 let handler = async (m, { conn }) => {
-  let user = global.db.data.users[m.sender]
+let user = global.db.data.users[m.sender]
+const time = user.lastberburu + 2700000; // 45 minutos de espera
+if (new Date - user.lastberburu < 2700000) {
+return conn.reply(m.chat, Por favor, descansa un momento para seguir cazando.\n\n⫹⫺ Tiempo restante: ${clockString(time - new Date())}, m)
+}
 
-  // Lógica para iniciar la caza
-  let rsl = Math.floor(Math.random() * 500) // Yenes ganados
-  cooldowns[m.sender] = Date.now()
+// Lógica para iniciar la caza
+let rsl = Math.floor(Math.random() * 500) // Yenes ganados
+cooldowns[m.sender] = Date.now()
 
-  // Obtener el nombre del usuario o su número si no tiene nombre
-  let username = m.pushName || m.sender.split('@')[0]
+// Obtener el nombre del usuario o su número si no tiene nombre
+let username = m.pushName || m.sender.split('@')[0]
 
-  // Enviar mensajes secuenciales con el nombre o número del usuario
-  setTimeout(() => {
-    conn.reply(m.chat, `@${m.sender.split('@s.whatsapp.net')[0]} *¡Objetivo en radar! 🧚‍♂️🎯*`, null, { mentions: [m.sender] })
-  }, 20000);
+// Enviar mensajes secuenciales con el nombre o número del usuario
+setTimeout(() => {
+conn.reply(m.chat, @${m.sender.split('@s.whatsapp.net')[0]} *¡Objetivo en radar! 🧚‍♂️🎯*, null, { mentions: [m.sender] })
+}, 20000);
 
-  setTimeout(() => {
-    conn.reply(m.chat, `@${m.sender.split('@s.whatsapp.net')[0]} *¡Preparación para la caza! 🗡️*`, null, { mentions: [m.sender] })
-  }, 18000);
+setTimeout(() => {
+conn.reply(m.chat, @${m.sender.split('@s.whatsapp.net')[0]} *¡Preparación para la caza! 🗡️*, null, { mentions: [m.sender] })
+}, 18000);
 
-  setTimeout(() => {
-    conn.reply(m.chat, `@${m.sender.split('@s.whatsapp.net')[0]} *¡Duende detectado! 🧚‍♂️*`, null, { mentions: [m.sender] })
-  }, 15000);
+setTimeout(() => {
+conn.reply(m.chat, @${m.sender.split('@s.whatsapp.net')[0]} *¡Duende detectado! 🧚‍♂️*, null, { mentions: [m.sender] })
+}, 15000);
 
-  // Enviar la imagen
-  const imageUrl = "https://qu.ax/atpzr.jpeg" // Reemplaza con la URL de la imagen que desees mostrar
-  setTimeout(() => {
-    conn.sendImage(m.chat, imageUrl, `¡Has cazado un duende y ganado *${toNum(rsl)}* yenes! Ahora tienes un total de *${toNum(user.coin + rsl)}* yenes. 💸`, m)
-  }, 22000);
+// Enviar la imagen
+const imageUrl = "https://qu.ax/atpzr.jpeg" // Reemplaza con la URL de la imagen que desees mostrar
+setTimeout(() => {
+conn.sendImage(m.chat, imageUrl, ¡Has cazado un duende y ganado *${toNum(rsl)}* yenes! Ahora tienes un total de *${toNum(user.coin + rsl)}* yenes. 💸, m)
+}, 22000);
 
-  // Actualiza el tiempo de la última caza
-  user.lastberburu = new Date * 1;
+// Actualiza el tiempo de la última caza
+user.lastberburu = new Date * 1;
 }
 
 handler.help = ['cazar', 'cazar_duende', 'cazar_duendes']
@@ -42,11 +46,19 @@ handler.register = true
 export default handler
 
 function toNum(number) {
-  if (number >= 1000 && number < 1000000) {
-    return (number / 1000).toFixed(1) + 'k'
-  } else if (number >= 1000000) {
-    return (number / 1000000).toFixed(1) + 'M'
-  } else {
-    return number.toString()
-  }
+if (number >= 1000 && number < 1000000) {
+return (number / 1000).toFixed(1) + 'k'
+} else if (number >= 1000000) {
+return (number / 1000000).toFixed(1) + 'M'
+} else {
+return number.toString()
 }
+}
+
+function clockString(ms) {
+let seconds = Math.floor(ms / 1000)
+let minutes = Math.floor(seconds / 60)
+seconds = seconds % 60
+return ${minutes} minutos y ${seconds} segundos
+}
+
