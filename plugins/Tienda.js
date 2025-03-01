9a6d915c73c693b18a09aef5218efe8c953fe577
Aqua200 <chinquepapa@gmail.com>
2025-02-28 22:44:56 -0800

    Update Tienda.js

--- plugins/Tienda.js
+++ plugins/Tienda.js
@@ -3,17 +3,26 @@ let handlerBuy = async (m, { conn, usedPrefix }) => {
     let user = global.db.data.users[who];
 
     let precio = 1000;  // El precio de una waifu
-
     if (user.coin < precio) {
         return conn.reply(m.chat, `No tienes suficiente ${moneda} para comprar una waifu.`, m);
     }
 
     user.coin -= precio;  // Resta el dinero de la cartera
-    let waifu = `Waifu ${user.waifus.length + 1}`;  // Aquí puedes cambiar por el nombre de la waifu deseada
     if (!user.waifus) user.waifus = [];  // Asegúrate de que el usuario tenga un arreglo de waifus
+
+    // Nombre y URL de la waifu que deseas añadir
+    let waifuName = 'Waifu Sakura';  // Aquí puedes cambiar el nombre de la waifu
+    let waifuImageUrl = 'https://ejemplo.com/imagen-sakura.jpg';  // Aquí puedes cambiar la URL de la imagen de la waifu
+
+    // Crea un objeto para la waifu con nombre y URL de la imagen
+    let waifu = {
+        name: waifuName,
+        imageUrl: waifuImageUrl
+    };
+
     user.waifus.push(waifu);  // Añade la waifu al inventario del usuario
 
-    conn.reply(m.chat, `¡Has comprado una waifu! Ahora tienes: ${user.waifus.join(', ')}`, m);
+    conn.reply(m.chat, `¡Has comprado a ${waifuName}! Ahora tienes: ${user.waifus.map(w => w.name).join(', ')}`, m);
 }
 
 handlerBuy.help = ['comprarwaifu'];
