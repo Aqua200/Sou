1d84e4aecdb4d34ad715d21c371ef7653e4e525e
Aqua200 <chinquepapa@gmail.com>
2025-03-02 17:16:27 -0800

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,49 +1,26 @@
-import { WAMessageStubType } from '@whiskeysockets/baileys';
-import fetch from 'node-fetch';
+import {WAMessageStubType} from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
 
-// Cache para imágenes por defecto y mensajes
-const DEFAULT_IMAGE = 'https://files.catbox.moe/xr2m6u.jpg';
-const WELCOME_MSG = '❀ *Bienvenido* a {{groupName}}\n ✰ {{taguser}}\n{{welcomeText}}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.';
-const BYE_MSG = '❀ *Adiós* de {{groupName}}\n ✰ {{taguser}}\n{{byeText}}\n •(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.';
+export async function before(m, {conn, participants, groupMetadata}) {
+  if (!m.messageStubType || !m.isGroup) return !0;
+    let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
+    let pp2 = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
+  let img = await (await fetch(`${pp}`)).buffer()
+  let img2 = await (await fetch(`${pp2}`)).buffer()
 
-export async function before(m, { conn, participants, groupMetadata }) {
-  // Verificación rápida para salir si no es necesario procesar
-  if (!m.messageStubType || !m.isGroup) return true;
+  let chat = global.db.data.chats[m.chat]
 
-  const who = m.messageStubParameters[0];
-  const taguser = `@${who.split('@')[0]}`;
-  const chat = global.db.data.chats[m.chat];
-
-  // Si no hay bienvenida activada, salir
-  if (!chat.welcome) return true;
-
-  // Obtener imagen de perfil o usar la predeterminada
-  let img;
-  try {
-    const pp = await conn.profilePictureUrl(who, 'image');
-    img = await (await fetch(pp, { timeout: 5000 })).buffer(); // Timeout para evitar bloqueos
-  } catch {
-    img = await (await fetch(DEFAULT_IMAGE, { timeout: 5000 })).buffer();
+  if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+    let wel = `❀ *Bienvenido* a ${groupMetadata.subject}\n ✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+await conn.sendMini(m.chat, packname, dev, wel, img, img, channel, fkontak)
   }
 
-  // Preparar mensaje según el evento
-  const groupName = groupMetadata.subject;
-  if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-    const message = WELCOME_MSG
-      .replace('{{groupName}}', groupName)
-      .replace('{{taguser}}', taguser)
-      .replace('{{welcomeText}}', global.welcom1 || '');
-    await conn.sendMessage(m.chat, { image: img, caption: message, mentions: [who] });
-  } else if (
-    m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE ||
-    m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE
-  ) {
-    const message = BYE_MSG
-      .replace('{{groupName}}', groupName)
-      .replace('{{taguser}}', taguser)
-      .replace('{{byeText}}', global.welcom2 || '');
-    await conn.sendMessage(m.chat, { image: img, caption: message, mentions: [who] });
+  if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
+   let bye = `❀ *Adiós* de ${groupMetadata.subject}\n ✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+await conn.sendMini(m.chat, packname, dev, bye, img2, img2, channel, fkontak)
   }
 
-  return true;
-}
+  if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE) {
+   let bye = `❀ *Adiós* de ${groupMetadata.subject}\n ✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+await conn.sendMini(m.chat, packname, dev, bye, img2, img2, channel, fkontak)
+  }}
