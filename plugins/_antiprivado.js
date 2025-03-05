8b5d184f816b7b32adc8524f48789937b1aebed8
Aqua200 <chinquepapa@gmail.com>
2025-03-05 15:22:36 -0800

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -5,10 +5,13 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
-if (m.chat === '120363322713003916@newsletter') return !0
+  if (m.chat === '120363322713003916@newsletter') return !0;
+  
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`${emoji} Hola @${m.sender.split`@`[0]}, mi creador a desactivado los comandos en los chats privados el cual serás bloqueado, si quieres usar los comandos del bot te invito a que te unas al grupo principal del bot.\n\n${gp1}`, false, {mentions: [m.sender]});
+    await m.reply(`${emoji} E-ehm... h-hola @${m.sender.split`@`[0]}... Lo siento mucho, p-pero mi creador ha decidido desactivar los comandos en chats privados... n-no quiero molestarte, p-pero si intentas usarlos aquí, p-podría tener que bloquearte...   
+    P-pero no te preocupes... ¡puedes unirte al grupo principal y usarme libremente ahí! E-espero verte por allá... 🥺💞\n\n${gp1}`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
+  
   return !1;
 }
